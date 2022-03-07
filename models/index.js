fe4a96c95643d013e4b404cf223be3b2b0557bee
Sanjeev Kumar <sanjeevkumar@me.com>
2022-03-07 16:32:44 +1100

    Minor change to appointments model relationship in index file.

--- models/index.js
+++ models/index.js
@@ -1,34 +1,23 @@
 const Patients = require('./Patients');
 const Doctors = require('./Doctors');
-// const Appointments = require ('./Appointments');
+const Appointments = require ('./Appointments');
 
 
-// Patients.hasMany(Appointments, {
-//     foreignKey: 'patients_id',
-// });
+Patients.hasMany(Appointments, {
+    foreignKey: 'patients_id',
+});
 
-// Doctors.hasMany(Appointments, {
-//     foreignKey: 'doctors_id',
+Doctors.hasMany(Appointments, {
+    foreignKey: 'doctors_id',
+});
 
-// });
+Appointments.belongsTo(Patients, {
+    foreignKey: 'patients_id',
+});
 
-// Appointments.belongsTo(Patients, {
-//     foreignKey: 'patients_id',
-// });
-
-// Appointments.belongsTo(Doctors, {
-//     foreignKey: 'doctors_id',
-// });
-
-// Appointments.hasOne(Patients, {
-//     foreignKey: "patients_id",
-
-// });
-
-// Appointments.hasOne(Doctors, {
-//     foreignKey: "doctors_id",
-
-// });
+Appointments.belongsTo(Doctors, {
+    foreignKey: 'doctors_id',
+});
 
 module.exports = { Patients, Doctors };
 
