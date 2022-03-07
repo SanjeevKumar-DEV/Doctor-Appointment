b997a1a9a48035963a9803edd96f5fcb7bb90536
AndyNg0712 <annguyen_2002@yahoo.com>
2022-03-07 13:09:02 +1100

    update model_index

--- models/index.js
+++ models/index.js
@@ -3,32 +3,32 @@ const Doctors = require('./Doctors');
 const Appointments = require ('./Appointments');
 
 
-// Patients.hasMany(Appointments, {
-//     foreignKey: 'patients_id',
-// });
+Patients.hasMany(Appointments, {
+    foreignKey: 'patients_id',
+});
 
-// Doctors.hasMany(Appointments, {
-//     foreignKey: 'doctors_id',
+Doctors.hasMany(Appointments, {
+    foreignKey: 'doctors_id',
 
-// });
+});
 
-// Appointments.belongsTo(Patients, {
-//     foreignKey: 'patients_id',
-// });
+Appointments.belongsTo(Patients, {
+    foreignKey: 'patients_id',
+});
 
-// Appointments.belongsTo(Doctors, {
-//     foreignKey: 'doctors_id',
-// });
+Appointments.belongsTo(Doctors, {
+    foreignKey: 'doctors_id',
+});
 
-// Appointments.hasOne(Patients, {
-//     foreignKey: "patients_id",
+Appointments.hasOne(Patients, {
+    foreignKey: "patients_id",
 
-// });
+});
 
-// Appointments.hasOne(Doctors, {
-//     foreignKey: "doctors_id",
+Appointments.hasOne(Doctors, {
+    foreignKey: "doctors_id",
 
-// });
+});
 
 module.exports = { Patients, Doctors, Appointments};
 
