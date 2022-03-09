aef3734580407bdaaa9e4be155d6e659eb67059c
Sanjeev Kumar <sanjeevkumar@me.com>
2022-03-09 22:02:40 +1100

    Added handlebar to display list of doctors on homepage as clickable link.

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,8 +1,23 @@
-const router = require("express").Router();
+const router = require('express').Router();
+const { Doctors, Patients } = require('../models');
+const withAuth = require('../utils/auth');
 
-// route to get home page for patient login
-router.get("/", async (req, res) => {
-  res.render("login", {});
+router.get('/', async (req, res) => {
+  try {
+    // Get all doctors
+    const doctorData = await Doctors.findAll();
+
+    // Serialize data so the template can read it
+    const doctors = doctorData.map((doctor) => doctor.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render('homepage', { 
+      doctors, 
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
