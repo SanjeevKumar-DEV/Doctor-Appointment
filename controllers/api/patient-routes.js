27497e3720b7906d15a1d5a52fec2c2f811226c1
Sanjeev Kumar <sanjeevkumar@me.com>
2022-03-11 20:47:37 +1100

    Added login and logout code.

--- controllers/api/patient-routes.js
+++ controllers/api/patient-routes.js
@@ -66,4 +66,47 @@ router.post("/", async (req, res) => {
   }
 });
 
+router.post('/login', async (req, res) => {
+  try {
+    const patientData = await Patients.findOne({ where: { email: req.body.email } });
+
+    if (!patientData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+
+    const validPassword = await patientData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = patientData.patients_id;
+      req.session.logged_in = true;
+      
+      res.json({ user: patientData, message: 'You are now logged in!' });
+    });
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.post('/logout', (req, res) => {
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
+
 module.exports = router;
